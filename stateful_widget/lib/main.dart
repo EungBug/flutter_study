1daeed350f5494d1c7afca8ddfee87c122e73890
EungBug <eungb14@gmail.com>
2023-12-20 20:52:22 +0900

    ✨ BuildContext

--- stateful_widget/lib/main.dart
+++ stateful_widget/lib/main.dart
@@ -12,45 +12,45 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
-
-  void onClicked() {
-    // State가 변경됨을 알려 build()가 다시 실행되도록 하여 ui를 업데이트함
-    setState(() {
-      counter += 1;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFFf4EDDB),
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.w600,
+            color: Colors.red,
+          ),
+        ),
+      ),
+      home: const Scaffold(
+        backgroundColor: Color(0xFFf4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              Text(
-                '$counter',
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              IconButton(
-                  onPressed: onClicked,
-                  iconSize: 40,
-                  icon: const Icon(
-                    Icons.add_box_rounded,
-                  ))
+              MyLargeTitle(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: Theme.of(context).textTheme.titleLarge!.fontSize,
+        color: Theme.of(context).textTheme.titleLarge!.color,
       ),
     );
   }
