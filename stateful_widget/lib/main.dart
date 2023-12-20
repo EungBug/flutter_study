3e3bb666f44a0f6a68259affe3aa3c0fd59d8713
EungBug <eungb14@gmail.com>
2023-12-20 20:37:52 +0900

    ✨ StatefulWidget 작성해보기

--- stateful_widget/lib/main.dart
+++ stateful_widget/lib/main.dart
@@ -4,14 +4,54 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    // State가 변경됨을 알려 build()가 다시 실행되도록 하여 ui를 업데이트함
+    setState(() {
+      counter += 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFf4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              IconButton(
+                  onPressed: onClicked,
+                  iconSize: 40,
+                  icon: const Icon(
+                    Icons.add_box_rounded,
+                  ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
