a31406839f17ea13f5cef39cd2916f9e303f5f3d
EungBug <eungb14@gmail.com>
2023-12-21 20:35:54 +0900

    💄 Theme 설정

--- challenge_pomodoro/lib/main.dart
+++ challenge_pomodoro/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home_screen.dart';
+
 void main() {
   runApp(const App());
 }
@@ -9,6 +11,20 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          displaySmall: TextStyle(
+            color: Colors.white.withOpacity(0.5),
+          ),
+        ),
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE64D3D),
+        ),
+        primaryColor: const Color(0xFFE64D3D),
+        fontFamily: 'esamanru',
+      ),
+      home: const HomeScreen(),
+    );
   }
 }
